760ec6ba58e90c7757105af4f527cd8b772a3ca6
gd <haotianr@gmail.com>
2018-04-28 21:39:26 -0400

    estimator

--- card_detector.py
+++ card_detector.py
@@ -13,60 +13,109 @@ parser.add_argument('--batch_size', default=100, type=int, help='batch size')
 parser.add_argument('--train_steps', default=1000, type=int,
                     help='number of training steps')
 
+					
+def card_classify_fn(features, labels, mode):
+	"""CNN model to detect a card"""
+	# input layer
+	input_layer = tf.reshape(features["x"], [-1, 57, 57, 3])
+	
+	# conv1
+	# input shape [batch_size, 57, 57, 3]
+	# output shape [batch_size, 57, 57, 32]
+	conv1 = tf.layers.conv2d(
+		inputs=input_layer,
+		filters=32,
+		kernel_size=[5, 5],
+		padding='same',
+		activation=tf.nn.relu)
+	
+	# pool1
+	# output shape [batch_size, 19, 19, 32]
+	pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], stride=3)
+	
+	# pool2
+	# output shape [batch_size, 19, 19, 64]
+	conv2 = tf.layers.conv2d(
+		inputs=pool1,
+		filters=64,
+		kernel_size=[5,5],
+		padding='same',
+		activation=tf.nn.relu)
+	
+	# pool2
+	# output shape [batch_size, 9, 9, 64]
+	pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], stride=2)
+	
+	pool2_flat = tf.reshape(pool2, [-1, 9 * 9 * 64])
+	
+	dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+	dropout = tf.layers.dropout(
+		inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+	logits = tf.layers.dense(inputs=dropout, unit=4)
+	
+	predictions = {
+		"classes": tf.argmax(input=logits, axis=1),
+		"probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+	}
+	if mode == tf.estimator.ModeKeys.PREDICT:
+		return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+		
+	# calculate loss
+	loss = tf.loess.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+	
+	if mode == tf.estimator.ModeKeys.TRAIN:
+		optimizer = tf.train.GradientDescentOptimizier(learning_rate=0.001)
+		train_op = optimizer.minimize(
+			loss=loess,
+			global_step=tf.train.get_global_step())
+		return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+		
+	eval_metrics_ops = {
+		"accuracy": tf.metrics.accuracy(
+			labels=labels, predictions=predictions["classes"])}
+	return tf.estimator.EstimatorSpec(
+		mode=mode, loss=loss, eval_metrics_ops=eval_metrics_ops)
+					
 def main(argv):
-    args = parser.parse_args(argv[1:])
+	args = parser.parse_args(argv[1:])
 
-    # Fetch the data
-    (train_x, train_y), (test_x, test_y) = iris_data.load_data()
+	# Create Estimator
+	classifier = tf.estimator.Estimator(
+		model_fn = card_classify_fn, model_dir="D:\TF\card")
 
-    # Feature columns describe how to use the input.
-    my_feature_columns = []
-    for key in train_x.keys():
-        my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+	# Train the Model.
+	classifier.train(
+		input_fn=data_loader.train_input_fn,
+		steps=args.train_steps)
 
-    # Build 2 hidden layer DNN with 10, 10 units respectively.
-    classifier = tf.estimator.DNNClassifier(
-        feature_columns=my_feature_columns,
-        # Two hidden layers of 10 nodes each.
-        hidden_units=[10, 10],
-        # The model must choose between 3 classes.
-        n_classes=3)
+	# TODO: create real eval and Evaluate the model.
+	eval_result = classifier.evaluate(
+		input_fn=data_loader.train_input_fn)
 
-    # Train the Model.
-    classifier.train(
-        input_fn=lambda:iris_data.train_input_fn(train_x, train_y,
-                                                 args.batch_size),
-        steps=args.train_steps)
-
-    # Evaluate the model.
-    eval_result = classifier.evaluate(
-        input_fn=lambda:iris_data.eval_input_fn(test_x, test_y,
-                                                args.batch_size))
-
-    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+	print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
 
     # Generate predictions from the model
-    expected = ['Setosa', 'Versicolor', 'Virginica']
-    predict_x = {
-        'SepalLength': [5.1, 5.9, 6.9],
-        'SepalWidth': [3.3, 3.0, 3.1],
-        'PetalLength': [1.7, 4.2, 5.4],
-        'PetalWidth': [0.5, 1.5, 2.1],
-    }
+    #expected = ['Setosa', 'Versicolor', 'Virginica']
+    #predict_x = {
+    #    'SepalLength': [5.1, 5.9, 6.9],
+    #    'SepalWidth': [3.3, 3.0, 3.1],
+    #    'PetalLength': [1.7, 4.2, 5.4],
+    #    'PetalWidth': [0.5, 1.5, 2.1],
+    #}
 
-    predictions = classifier.predict(
-        input_fn=lambda:iris_data.eval_input_fn(predict_x,
-                                                labels=None,
-                                                batch_size=args.batch_size))
+    #predictions = classifier.predict(
+    #   input_fn=lambda:iris_data.eval_input_fn(predict_x,
+    #                                            labels=None,
+    #                                           batch_size=args.batch_size))
 
-    template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
+    #template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
 
-    for pred_dict, expec in zip(predictions, expected):
-        class_id = pred_dict['class_ids'][0]
-        probability = pred_dict['probabilities'][class_id]
+    #for pred_dict, expec in zip(predictions, expected):
+    #   class_id = pred_dict['class_ids'][0]
+    #    probability = pred_dict['probabilities'][class_id]
 
-        print(template.format(iris_data.SPECIES[class_id],
-                              100 * probability, expec))
+    #   print(template.format(iris_data.SPECIES[class_id],
+    #                          100 * probability, expec))
 
 
 if __name__ == '__main__':
--- data_loader.py
+++ data_loader.py
@@ -3,38 +3,21 @@ import numpy as np
 from scipy import misc
 import glob
 
-TRAIN_DIR = "http://download.tensorflow.org/data/iris_training.csv"
-TEST_DIR = "http://download.tensorflow.org/data/iris_test.csv"
-
-CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth',
-                    'PetalLength', 'PetalWidth', 'Species']
-SPECIES = ['Setosa', 'Versicolor', 'Virginica']
-
-def load_data(y_name='Species'):
-    """Return as (train_x, train_y), (test_x, test_y)."""
-    train_path, test_path = maybe_download()
-
-    train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
-    train_x, train_y = train, train.pop(y_name)
-
-    test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
-    test_x, test_y = test, test.pop(y_name)
-
-    return (train_x, train_y), (test_x, test_y)
-
-
+TRAIN_DIR = "..\card_*.png"
+TEST_DIR = "..\card_*.png"
+	
 def train_input_fn(features, labels, batch_size):
-    """An input function for training"""
-    # Convert the inputs to a Dataset.
-    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+	"""An input function for training"""
+	train_file_list = glob.glob(TRAIN_DIR)
+	a = [tf.image.resize_image_with_crop_or_pad(tf.image.decode_png(x, 3), 57, 57) for x in train_file_list]
+	return tf.estimator.inputs.numpy_input_fn(
+	{"x" : np.array(a)},
+		np.array(['1hdsc'.find(x[8]) for x in train_file_list]),
+		batch_size = 128,
+		num_epochs = 3,
+		shuffle = True)
 
-    # Shuffle, repeat, and batch the examples.
-    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
-
-    # Return the dataset.
-    return dataset
-
-
+# TODO: create real eval func
 def eval_input_fn(features, labels, batch_size):
     """An input function for evaluation or prediction"""
     features=dict(features)
@@ -55,59 +38,9 @@ def eval_input_fn(features, labels, batch_size):
     return dataset
 
 
-# The remainder of this file contains a simple example of a csv parser,
-#     implemented using a the `Dataset` class.
-
-# `tf.parse_csv` sets the types of the outputs to match the examples given in
-#     the `record_defaults` argument.
-CSV_TYPES = [[0.0], [0.0], [0.0], [0.0], [0]]
-
-def _parse_line(line):
-    # Decode the line into its fields
-    fields = tf.decode_csv(line, record_defaults=CSV_TYPES)
-
-    # Pack the result into a dictionary
-    features = dict(zip(CSV_COLUMN_NAMES, fields))
-
-    # Separate the label from the features
-    label = features.pop('Species')
-
-    return features, label
-
-
-def csv_input_fn(csv_path, batch_size):
-    # Create a dataset containing the text lines.
-    dataset = tf.data.TextLineDataset(csv_path).skip(1)
-
-    # Parse each line.
-    dataset = dataset.map(_parse_line)
-
-    # Shuffle, repeat, and batch the examples.
-    dataset = dataset.shuffle(1000).repeat().batch(batch_size)
-
-    # Return the dataset.
-    return dataset
-
 def main():
-	train_file_list = glob.glob('..\card_*.png')
-	a = [tf.image.resize_image_with_crop_or_pad(tf.image.decode_png(x, 3), 57, 57) for x in train_file_list]
-	tf.estimator.inputs.numpy_input_fn(
-		np.array(a),
-		np.array([x[8] for x in train_file_list]),
-		batch_size = 128,
-		num_epochs = 3,
-		shuffle = True,
-		)
-		
-	#for name in train_file_list:
-		#load, decode and crop
-		#image_full_tensor = tf.image.decode_png(name, 3)
-		#image_tensor = tf.image.resize_image_with_crop_or_pad(image_full_tensor, 57, 57)
-		
-		#print(image_tensor.shape)
-	
-		
-	test_file_list = glob.glob('..\card_1*.png')
+	train_file_list = glob.glob(TRAIN_DIR)
+	pass
 	
 if __name__ == '__main__':
 	main()
